33f1eaf0827d7ee03d916d8640ce506e4932475d
sema <semasenturk00@hotmail.com>
2025-04-25 09:15:45 -0400

    SemaDeneme Updated

--- src/Deneme_1/Sema24.java
+++ src/Deneme_1/Sema24.java
@@ -5,5 +5,7 @@ public class Sema24 {
     public static void main(String[] args) {
 
         System.out.println("hello from new jersey");
+
+        System.out.println("I'm learning java");
     }
 }
